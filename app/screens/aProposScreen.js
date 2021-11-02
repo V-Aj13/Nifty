5f8813df46bab8004da286fd5f09c36c69019fc4
JACOB-Leonard <shinwa.lj@gmail.com>
2021-11-02 16:12:14 +0100

    petit message

--- app/screens/aProposScreen.js
+++ app/screens/aProposScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 function aProposScreen(props) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>on s'amuse</Text>
+          <Text>on s'amuse bien </Text>
         </View>
       );
 }
